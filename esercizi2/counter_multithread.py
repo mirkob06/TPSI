165a7e19c39f0aa551ad10bcdf5573fc83b69982
mirkob06 <mirko.buonocore@students.sarrocchi.it>
2024-11-15 12:17:15 +0100

    modified the file counter_multithread.py adding the semaphore

--- esercizi2/counter_multithread.py
+++ esercizi2/counter_multithread.py
@@ -1,14 +1,18 @@
 import threading
 import time
 
+semaphore = threading.Semaphore (0)
+
 # Funzione per contare in avanti
 def count_up():
     for i in range(1, 11):
         print(f"count up: {i}")
         time.sleep(1)
+    semaphore.release()
 
 # Funzione per contare all'indietro
 def count_down():
+    semaphore.acquire()
     for i in range(10, 0, -1):
         print(f"count down: {i}")
         time.sleep(1)
