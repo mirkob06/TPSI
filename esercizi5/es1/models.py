8cfdd4645e0bef135bd816b7e0ae29d6f54925b6
mirkob06 <mirko.buonocore@students.sarrocchi.it>
2025-03-12 10:56:01 +0100

    added models in es1

---
+++ esercizi5/es1/models.py
@@ -0,0 +1,13 @@
+class UserMOdel:
+    def __init__ (self, id, name, company, username, email, address, zip, state, country, phone, photo):
+        self.id = id
+        self.name = name
+        self.company = company
+        self.username = username
+        self.email = email
+        self.address = address
+        self.zip = zip
+        self.state = state
+        self.country = country
+        self.phone = phone
+        self.photo = photo
