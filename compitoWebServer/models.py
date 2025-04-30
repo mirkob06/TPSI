c5381c576603903337969caacaa7be578ede6d0a
mirkob06 <mirko.buonocore06@gmail.com>
2025-04-30 09:08:18 +0200

    modified the file models.py and added the database in it

--- compitoWebServer/models.py
+++ compitoWebServer/models.py
@@ -1,12 +1,27 @@
-from sqlalchemy import Column, Integer, String, Date 
-from database import Base 
+from sqlalchemy import Column, Integer, String, Float, Date, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./cities.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+Base = declarative_base()
 
 class City(Base):
-    __tablename__ = 'cities'
-    
+    __tablename__ = "cities"
+
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    country = Column(String, index=True)
-    population = Column(Integer)
-    area = Column(Integer)
-    date_added = Column(Date)
+    name = Column(String, nullable=False)
+    country = Column(String, nullable=False)
+    population = Column(Integer, nullable=False)
+    area = Column(Float, nullable=False)
+    last_survey = Column(Date, nullable=False)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
